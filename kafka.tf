b9778df43461aac5a91b75c7d98a88a0841d61c5
igorsr <igor.souza.russo@gmail.com>
2024-05-01 10:38:57 -0300

    [fix] (Kafka): Changed init strategy.

--- kafka.tf
+++ kafka.tf
@@ -31,28 +31,49 @@ resource "aws_security_group" "kafka_sg" {
 # Define EC2 instances for Kafka 
 resource "aws_instance" "kafka_instance" {
   ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t3.large"
+  instance_type   = "t3.medium"
   security_groups = [aws_security_group.kafka_sg.name]
   key_name        = "realtime"
 
-  user_data = <<-EOF
-    #!/bin/bash
-    # Install Java
-    sudo yum update -y
-    sudo yum install -y java-1.8.0-openjdk-devel
-    
-    # Install Kafka
-    wget https://downloads.apache.org/kafka/3.7.0/kafka_2.12-3.7.0.tgz
-    tar -xvf kafka_2.12-3.7.0.tgz
-    cd kafka_2.12-3.7.0
+  provisioner "local-exec" {
+    command = <<EOH
+        #!/bin/bash
+        # Install Java
+        sudo yum update -y
+        sudo yum install -y java-1.8.0-openjdk-devel
+        
+        # Install Kafka
+        wget https://downloads.apache.org/kafka/3.7.0/kafka_2.12-3.7.0.tgz
+        tar -xvf kafka_2.12-3.7.0.tgz
+        cd kafka_2.12-3.7.0
 
-    export KAFKA_HEAP_OPTS="-Xmx4G -Xms4G"
+        export KAFKA_HEAP_OPTS="-Xmx2G -Xms2G"
 
-    nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
-    
-    # Start Kafka server (this is just an example, you may need to modify it based on your Kafka setup)
-    nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
-  EOF
+        nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
+        
+        # Start Kafka server (this is just an example, you may need to modify it based on your Kafka setup)
+        nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
+        EOH
+  }
+
+  # user_data = <<-EOF
+  #   #!/bin/bash
+  #   # Install Java
+  #   sudo yum update -y
+  #   sudo yum install -y java-1.8.0-openjdk-devel
+  #   
+  #   # Install Kafka
+  #   wget https://downloads.apache.org/kafka/3.7.0/kafka_2.12-3.7.0.tgz
+  #   tar -xvf kafka_2.12-3.7.0.tgz
+  #   cd kafka_2.12-3.7.0
+
+  #   export KAFKA_HEAP_OPTS="-Xmx4G -Xms4G"
+
+  #   nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
+  #   
+  #   # Start Kafka server (this is just an example, you may need to modify it based on your Kafka setup)
+  #   nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
+  # EOF
 
   tags = {
     Name = "Kafka Instance"
