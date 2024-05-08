8088fb7b622ea0e10b619a88bd274f2974cc4e31
igor-russo-geo <igor.souza.russo@gmail.com>
2024-05-08 18:48:34 -0300

    teste!

--- kafka.tf
+++ kafka.tf
@@ -1,73 +1,73 @@
 
-# resource "aws_security_group" "sg" {
-#   vpc_id = aws_vpc.vpc.id
-# }
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
+}
 
-# resource "aws_kms_key" "kms" {
-#   description = "example"
-# }
+resource "aws_kms_key" "kms" {
+  description = "example"
+}
 
-# resource "aws_cloudwatch_log_group" "test" {
-#   name = "msk_broker_logs"
-# }
+resource "aws_cloudwatch_log_group" "test" {
+  name = "msk_broker_logs"
+}
 
-# resource "aws_s3_bucket" "bucket" {
-#   bucket = "igorsr-msk-broker-logs-bucket"
-# }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "igorsr-msk-broker-logs-bucket"
+}
 
 # resource "aws_s3_bucket_acl" "bucket_acl" {
 #   bucket = aws_s3_bucket.bucket.id
 # }
 
 
-# resource "aws_msk_cluster" "poc_cluster" {
-#   cluster_name           = "example"
-#   kafka_version          = "3.2.0"
-#   number_of_broker_nodes = 2
+resource "aws_msk_cluster" "poc_cluster" {
+  cluster_name           = "example"
+  kafka_version          = "3.2.0"
+  number_of_broker_nodes = 2
 
-#   broker_node_group_info {
-#     instance_type = "kafka.t3.small"
-#     client_subnets = [
-#       aws_subnet.subnet_az1.id,
-#       aws_subnet.subnet_az2.id,
-#       aws_subnet.subnet_az3.id,
-#     ]
-#     storage_info {
-#       ebs_storage_info {
-#         volume_size = 1000
-#       }
-#     }
-#     security_groups = [aws_security_group.sg.id]
-#   }
+  broker_node_group_info {
+    instance_type = "kafka.t3.small"
+    client_subnets = [
+      aws_subnet.subnet_az1.id,
+      aws_subnet.subnet_az2.id,
+      aws_subnet.subnet_az3.id,
+    ]
+    storage_info {
+      ebs_storage_info {
+        volume_size = 1000
+      }
+    }
+    security_groups = [aws_security_group.sg.id]
+  }
 
-#   encryption_info {
-#     encryption_at_rest_kms_key_arn = aws_kms_key.kms.arn
-#   }
+  encryption_info {
+    encryption_at_rest_kms_key_arn = aws_kms_key.kms.arn
+  }
 
-#   logging_info {
-#     broker_logs {
-#       cloudwatch_logs {
-#         enabled   = true
-#         log_group = aws_cloudwatch_log_group.test.name
-#       }
-#       s3 {
-#         enabled = true
-#         bucket  = aws_s3_bucket.bucket.id
-#         prefix  = "logs/msk-"
-#       }
-#     }
-#   }
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = aws_cloudwatch_log_group.test.name
+      }
+      s3 {
+        enabled = true
+        bucket  = aws_s3_bucket.bucket.id
+        prefix  = "logs/msk-"
+      }
+    }
+  }
 
-#   tags = {
-#     environment = "dev"
-#   }
-# }
+  tags = {
+    environment = "dev"
+  }
+}
 
-# output "zookeeper_connect_string" {
-#   value = aws_msk_cluster.poc_cluster.zookeeper_connect_string
-# }
+output "zookeeper_connect_string" {
+  value = aws_msk_cluster.poc_cluster.zookeeper_connect_string
+}
 
-# output "bootstrap_brokers_tls" {
-#   description = "TLS connection host:port pairs"
-#   value       = aws_msk_cluster.poc_cluster.bootstrap_brokers_tls
-# }
+output "bootstrap_brokers_tls" {
+  description = "TLS connection host:port pairs"
+  value       = aws_msk_cluster.poc_cluster.bootstrap_brokers_tls
+}
