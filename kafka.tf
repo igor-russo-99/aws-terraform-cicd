9c72360c6770c600369de0e4a52e11f840483873
igor-russo-geo <igor.souza.russo@gmail.com>
2024-05-08 17:51:39 -0300

    [feat] (Kafka): Created msk cluster.

--- kafka.tf
+++ kafka.tf
@@ -1,110 +1,74 @@
-# # Create security group for Kafka
-# resource "aws_security_group" "kafka_sg" {
-#   name        = "kafka_sg"
-#   description = "Security group for Kafka"
 
-#   # Allow inbound traffic on Kafka port (default 9092)
-#   ingress {
-#     from_port   = 9092
-#     to_port     = 9092
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Adjust as per your network requirements
-#   }
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
+}
 
-#   # Allow inbound SSH traffic for administration purposes (optional)
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Restrict as per your requirements
-#   }
+resource "aws_kms_key" "kms" {
+  description = "example"
+}
 
-#   # Allow outbound traffic
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_cloudwatch_log_group" "test" {
+  name = "msk_broker_logs"
+}
 
-# # Create security group for ZooKeeper
-# resource "aws_security_group" "zookeeper_sg" {
-#   name        = "zookeeper_sg"
-#   description = "Security group for ZooKeeper"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "igorsr-msk-broker-logs-bucket"
+}
 
-#   # Allow inbound traffic on ZooKeeper port (default 2181)
-#   ingress {
-#     from_port   = 2181
-#     to_port     = 2181
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Adjust as per your network requirements
-#   }
-
-#   # Allow outbound traffic
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
 
 
-# resource "aws_instance" "zookeeper_instance" {
-#   ami             = data.aws_ami.ubuntu.id
-#   instance_type   = "t3.small"
-#   security_groups = [aws_security_group.zookeeper_sg.name]
+resource "aws_msk_cluster" "poc_cluster"{
+  cluster_name           = "example"
+  kafka_version          = "3.2.0"
+  number_of_broker_nodes = 3
 
-#   user_data = <<-EOF
-#     #!/bin/bash
-#     # Install Java
-#     sudo apt update -y
-#     sudo apt install -y openjdk-8-jdk
+  broker_node_group_info {
+    instance_type = "kafka.m5.large"
+    client_subnets = [
+      aws_subnet.subnet_az1.id,
+      aws_subnet.subnet_az2.id,
+      aws_subnet.subnet_az3.id,
+    ]
+    storage_info {
+      ebs_storage_info {
+        volume_size = 1000
+      }
+    }
+    security_groups = [aws_security_group.sg.id]
+  }
 
-#     # Install ZooKeeper
-#     wget https://downloads.apache.org/zookeeper/zookeeper-3.7.0/apache-zookeeper-3.7.0-bin.tar.gz
-#     tar -xzf apache-zookeeper-3.7.0-bin.tar.gz
-#     cd apache-zookeeper-3.7.0-bin
-#     
-#     # Start ZooKeeper server (this is just an example, you may need to modify it based on your ZooKeeper setup)
-#     nohup bin/zkServer.sh start > zookeeper.log 2>&1 &
-#   EOF
+  encryption_info {
+    encryption_at_rest_kms_key_arn = aws_kms_key.kms.arn
+  }
 
-#   tags = {
-#     Name = "ZooKeeper Instance"
-#   }
-# }
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = aws_cloudwatch_log_group.test.name
+      }
+      s3 {
+        enabled = true
+        bucket  = aws_s3_bucket.bucket.id
+        prefix  = "logs/msk-"
+      }
+    }
+  }
 
-# # Define EC2 instances for Kafka 
-# resource "aws_instance" "kafka_instance" {
-#   ami             = data.aws_ami.ubuntu.id
-#   instance_type   = "t3.medium"
-#   security_groups = [aws_security_group.kafka_sg.name]
-#   key_name        = "realtime"
+  tags = {
+    environment = "dev"
+  }
+}
 
-#   depends_on = [aws_instance.zookeeper_instance]
+output "zookeeper_connect_string" {
+  value = aws_msk_cluster.poc_cluster.zookeeper_connect_string
+}
 
-#   user_data = <<-EOF
-#     #!/bin/bash
-#     # Install Java
-#     sudo apt update -y
-#     sudo apt install -y openjdk-8-jdk
-#     
-#     # Install Kafka
-#     wget https://downloads.apache.org/kafka/3.7.0/kafka_2.12-3.7.0.tgz
-#     tar -xvf kafka_2.12-3.7.0.tgz
-#     cd kafka_2.12-3.7.0
-
-#     export KAFKA_HEAP_OPTS="-Xmx512M -Xms512M"
-
-#     #nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
-#     
-#     bin/zookeeper-server-start.sh -daemon config/zookeeper.properties && \
-#         bin/kafka-server-start.sh -daemon config/server.properties
-#   EOF
-
-#   tags = {
-#     Name = "Kafka Instance"
-#   }
-# }
+output "bootstrap_brokers_tls" {
+  description = "TLS connection host:port pairs"
+  value       = aws_msk_cluster.poc_cluster.bootstrap_brokers_tls
+}
